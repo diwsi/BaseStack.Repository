352ec4e314b268a7217573756e205ac08459155d
diwsi <xenamorphx@gmail.com>
2023-02-19 17:53:23 +0300

    initial

---
+++ IRepository.cs
@@ -0,0 +1,19 @@
+﻿using System.Linq.Expressions;
+
+namespace Repository
+{
+    public interface IRepository<T> where T:class
+    {
+        IEnumerable<T> Get(
+            Expression<Func<T, bool>>? filter = null,
+            Func<IQueryable<T>, IOrderedQueryable<T>>? orderBy = null,
+            string includeProperties = "");
+
+        T? GetByID(object id);
+
+        void Insert(T entity);
+        void Delete(object id);
+        void Update(T entityToUpdate);
+         
+    }
+}
